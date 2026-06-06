70ea50a7b2e09f5366675b29786d9822319dd5f6
Hikmet Gulsesli <hikmetgulsesli@gmail.com>
2026-06-06 10:17:44 +0300

    chore: auto-generate JSX screens from Stitch HTML

---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export { GameplaySignalrailLite } from "./GameplaySignalrailLite";
+export type { GameplaySignalrailLiteProps, GameplaySignalrailLiteActionId } from "./GameplaySignalrailLite";
+export { GameSettingsSignalrailLite } from "./GameSettingsSignalrailLite";
+export type { GameSettingsSignalrailLiteProps, GameSettingsSignalrailLiteActionId } from "./GameSettingsSignalrailLite";
